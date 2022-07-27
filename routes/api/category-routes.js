d89be7c97d38354279c0d464223a95729efae1ac
Garrett Lee <43485454+RGarrettLee@users.noreply.github.com>
2022-07-27 03:19:18 -0400

    fixed query

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -15,7 +15,7 @@ router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
   try {
-    const categoryData = await Category.findOneByPk(req.params.id, {
+    const categoryData = await Category.findByPk(req.params.id, {
       include: [{ model: Product }]
     });
     const category = categoryData.get({ plain: true });
