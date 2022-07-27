d32b0ded9da9069aa94df072c13a54d84023da6b
Garrett Lee <43485454+RGarrettLee@users.noreply.github.com>
2022-07-26 20:26:00 -0400

    fixes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,11 +3,11 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
   const categoryData = await Category.findAll({ include: [{ model: Product }]});
-  const categories = categoryData.get({ plain: true });
+  const categories = categoryData.map((category) => category.get({ plain: true }));
   res.status(200).json(categories);
 });
 
