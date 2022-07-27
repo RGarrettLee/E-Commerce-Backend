4742d945f27a31f3c52bec6b8bb82fd5154cd58d
Garrett Lee <43485454+RGarrettLee@users.noreply.github.com>
2022-07-26 20:23:19 -0400

    added async to routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,7 +4,7 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
   const productData = await Product.findAll({ include: [{ model: Category}, { model: Tag, through: ProductTag }]});
@@ -13,7 +13,7 @@ router.get('/', (req, res) => {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
   const productData = await Product.findByPk(req.params.id, { 
